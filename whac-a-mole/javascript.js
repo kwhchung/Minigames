aee32d8f0b3ded0a6e89faf7c28a89de9ff8ca66
kwhchung <78318019+kwhchung@users.noreply.github.com>
2022-05-02 04:54:09 +0800

    Update javascript.js

--- whac-a-mole/javascript.js
+++ whac-a-mole/javascript.js
@@ -1,12 +1,27 @@
 let score = 0;
-let count = 0;
+let seconds = 60;
 const map = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
 const scores = [1, 5, 10];
 const types = ["red", "yellow", "blue"];
 for(let i = 0; i < 9; i ++){
   document.getElementsByTagName("span")[i].style = "top: 101%";
 }
-setInterval(appear, 1000);
+let timeInterval = setInterval(time, 1000);
+let appearInterval = setInterval(appear, 1500);
+
+function time(){
+  seconds --;
+  document.getElementById("time").innerHTML = "Time: " + seconds;
+  if(seconds == 40){
+    clearInterval(appearInterval);
+    appearInterval = setInterval(appear, 1000);
+  }else if(seconds == 20){
+    clearInterval(appearInterval);
+    appearInterval = setInterval(appear, 750);
+  }else if(seconds == 0){
+    endGame();
+  }
+}
 
 function appear(){
   let i = 0;
@@ -25,7 +40,6 @@ function appear(){
   let type = Math.floor(Math.random() * 3);
   map[pos] = type;
   document.getElementsByTagName("span")[pos].style = "top: 0; transition: top 0.3s; background-color: " + types[type] + ";";
-  count ++;
   setTimeout(function(){disappear(pos);}, 2000);
 }
 
@@ -43,11 +57,26 @@ function hit(i){
   }
 }
 
+function endGame(){
+  clearInterval(timeInterval);
+  clearInterval(appearInterval);
+  for(let i = 0; i < 9; i ++){
+    document.getElementsByTagName("span")[i].style = "top: 101%";
+    map[i] = -1;
+  }
+}
+
 function restart(){
   score = 0;
+  seconds = 60;
+  clearInterval(appearInterval);
+  appearInterval = setInterval(appear, 1500);
+  clearInterval(timeInterval);
+  timeInterval = setInterval(time, 1000);
+  document.getElementById("time").innerHTML = "Time: " + seconds;
   document.getElementById("score").innerHTML = "Score: " + score;
   for(let i = 0; i < 9; i ++){
-    map[i] = -1;
     document.getElementsByTagName("span")[i].style = "top: 101%";
+    map[i] = -1;
   }
 }
